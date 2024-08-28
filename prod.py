0a67f04d4e4aeba97a4f1805223d07400c45a8ca
Pranav Kumar <96071514+PraNavKumAr01@users.noreply.github.com>
2024-08-28 08:28:18 +0530

    Update prod.py

--- prod.py
+++ prod.py
@@ -13,6 +13,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import os
 import time
@@ -127,7 +131,7 @@ def init_browser():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
 
 def get_search_results(search_query,list):
